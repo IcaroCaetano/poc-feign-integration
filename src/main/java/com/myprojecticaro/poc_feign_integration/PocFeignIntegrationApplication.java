5e3da885790485a7341f1a48f9018e11746aa13f
IcaroCaetano <icaro.caetano@gmail.com>
2025-10-07 19:02:27 -0300

    first commit

---
+++ src/main/java/com/myprojecticaro/poc_feign_integration/PocFeignIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_feign_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocFeignIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocFeignIntegrationApplication.class, args);
+	}
+
+}
