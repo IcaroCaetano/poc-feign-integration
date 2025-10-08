834fa09a7e97147f5e4e55e4cbf940a58c611d89
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-10-08 18:50:06 -0300

    Update AddressResponse.java

--- src/main/java/com/myprojecticaro/poc_feign_integration/dto/AddressResponse.java
+++ src/main/java/com/myprojecticaro/poc_feign_integration/dto/AddressResponse.java
@@ -1,1 +1,11 @@
+package com.myprojecticaro.poc_feign_integration.dto;
 
+public class AddressResponse {
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String localidade;
+    private String uf;
+
+    // getters e setters
+}
