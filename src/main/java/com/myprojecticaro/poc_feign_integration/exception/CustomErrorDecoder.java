6d876b35ce5962c26fbf93f51f285a97fd94446a
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-10-08 18:53:01 -0300

    Update CustomErrorDecoder.java

--- src/main/java/com/myprojecticaro/poc_feign_integration/exception/CustomErrorDecoder.java
+++ src/main/java/com/myprojecticaro/poc_feign_integration/exception/CustomErrorDecoder.java
@@ -1,1 +1,17 @@
+package com.myprojecticaro.poc_feign_integration.exception;
 
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomErrorDecoder implements ErrorDecoder {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomErrorDecoder.class);
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        log.error("Erro Feign: {} - status: {}", methodKey, response.status());
+        return new ApiException("Erro ao consultar ViaCEP. Código: " + response.status());
+    }
+}
