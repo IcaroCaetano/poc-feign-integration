2dcdb534b6fb2d425f4f09a0c5598b2182142053
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-10-08 18:48:55 -0300

    Update FeignConfig.java

--- src/main/java/com/myprojecticaro/poc_feign_integration/config/FeignConfig.java
+++ src/main/java/com/myprojecticaro/poc_feign_integration/config/FeignConfig.java
@@ -1,1 +1,13 @@
+package com.myprojecticaro.poc_feign_integration.config;
 
+import com.example.pocfeignintegration.exception.CustomErrorDecoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+
+public class FeignConfig {
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new CustomErrorDecoder();
+    }
+}
