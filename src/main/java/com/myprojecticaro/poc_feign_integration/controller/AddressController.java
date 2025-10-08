4be20be1c861d9c02c75223d71db398c8cdb309e
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-10-08 18:45:25 -0300

    Update AddressController.java

--- src/main/java/com/myprojecticaro/poc_feign_integration/controller/AddressController.java
+++ src/main/java/com/myprojecticaro/poc_feign_integration/controller/AddressController.java
@@ -1,1 +1,23 @@
+package com.myprojecticaro.poc_feign_integration.controller;
+
+import com.example.pocfeignintegration.dto.AddressResponse;
+import com.example.pocfeignintegration.service.AddressService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddressController {
+
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @GetMapping("/address/{cep}")
+    public AddressResponse getAddress(@PathVariable String cep) {
+        return addressService.getAddress(cep);
+    }
+}
 
