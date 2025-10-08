2229d8f73aaeb695db11007457eec48d416a5c86
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-10-08 18:51:19 -0300

    Update AddressService.java

--- src/main/java/com/myprojecticaro/poc_feign_integration/service/AddressService.java
+++ src/main/java/com/myprojecticaro/poc_feign_integration/service/AddressService.java
@@ -1,1 +1,32 @@
+package com.myprojecticaro.poc_feign_integration.service;
+
+import com.example.pocfeignintegration.client.ViaCepClient;
+import com.example.pocfeignintegration.dto.AddressResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+
+    private final ViaCepClient viaCepClient;
+
+    public AddressService(ViaCepClient viaCepClient) {
+        this.viaCepClient = viaCepClient;
+    }
+
+    @Retry(name = "viaCepRetry")
+    @CircuitBreaker(name = "viaCepCircuitBreaker", fallbackMethod = "fallbackAddress")
+    public AddressResponse getAddress(String cep) {
+        return viaCepClient.getAddressByCep(cep);
+    }
+
+    public AddressResponse fallbackAddress(String cep, Throwable t) {
+        AddressResponse response = new AddressResponse();
+        response.setCep(cep);
+        response.setLogradouro("Indisponível");
+        response.setLocalidade("Serviço fora do ar");
+        return response;
+    }
+}
 
